50bdb49db9cc29ab8421d5f0eb132f8b310eea84
fnand <fwschenck@gmail.com>
2023-09-13 23:26:15 +0200

    See if hook runs

--- scripts/ipynb_to_md.sh
+++ scripts/ipynb_to_md.sh
@@ -3,7 +3,8 @@
 # Copied from: https://gitlab.inria.fr/formations/cpp/gettingstartedwithmoderncpp/
 # This file is expected to be use as pre-commit git hook; copy it in .git/hooks/
 echo "This gets called"
-file_list=`git diff --diff-filter=ACM --name-only`
+file_list=`ls notebooks/*ipynb`
+#`git diff --diff-filter=ACM --name-only`
 
 for file in ${file_list}
 do
